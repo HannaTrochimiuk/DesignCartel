c3f42d8b015d06e5521271fee3492d584c2d1e23
Hanna Trochimiuk <trochimiuk.hanna@gmail.com>
2020-03-25 10:30:25 +0100

    improved valuesSection

--- src/aboutSection/ValueBox.js
+++ src/aboutSection/ValueBox.js
@@ -8,11 +8,11 @@ class ValueBox extends Component {
             <div className={this.props.className} onMouseEnter={this.props.onMouseEnter}>
                 <div className="value-box__side value-box__side--front">
                     <div className="value-icon-white">
-                        <img className='svg-size' src={this.props.iconWhite} />
+                        <img alt='icon' className='svg-size' src={this.props.iconWhite} />
                     </div>  
                 </div>   
                 <div className=
-                    {App.language == "pl" ? "value-box__side value-box__side--back pl"
+                    {App.language === "pl" ? "value-box__side value-box__side--back pl"
                     : "value-box__side value-box__side--back en"}>
                     <FormattedMessage id={this.props.id} />
                 </div>    
--- src/aboutSection/Values.js
+++ src/aboutSection/Values.js
@@ -5,7 +5,7 @@ import design from '../img/svg/design.svg';
 import people from '../img/svg/people.svg';
 import recycle from '../img/recycle.png';
 import { FormattedMessage } from 'react-intl'
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { CSSTransition } from 'react-transition-group';
 import ReactCSSTransitionReplace from 'react-css-transition-replace';
 
 
@@ -24,68 +24,45 @@ class Values extends Component {
         })
     };
 
-
     hideText = () => {
         this.setState({
             key: ' ',
             inProp: false
         })
     };
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         appearText: false
-    //     };
-    // }
-
-    // toggleAppear = () => {
-    //     this.setState({
-    //         appearText: false,
-    //         activeValue: null,
-
-    //     })
-    // };
-    // setActiveBox = (id) => {
-    //     this.setState({
-    //         activeValue: id,
-    //         appearText: true
-    //     })
-    // };
 
     render() {
         return (
             <div className='values-box' onMouseLeave={() => this.hideText()}>
                 <div className="values-circles"  >
                     <ValueBox
-                        className={this.state.key == "valueTextHuman" ? 'value-box value-box-active' : 'value-box'}
+                        className={this.state.key === "valueTextHuman" ? 'value-box value-box-active' : 'value-box'}
                         onMouseEnter={() => this.changeKey("valueTextHuman")}
                         iconWhite={people}
                         id="valueHuman" />
                     <ValueBox
-                        className={this.state.key == "valueTextDesign" ? 'value-box value-box-active' : 'value-box'}
+                        className={this.state.key === "valueTextDesign" ? 'value-box value-box-active' : 'value-box'}
                         onMouseEnter={() => this.changeKey("valueTextDesign")}
                         iconWhite={design}
                         id="valueDesign" />
                     <ValueBox
-                        className={this.state.key == "valueTextZeroWaste" ? 'value-box value-box-active' : 'value-box'}
+                        className={this.state.key === "valueTextZeroWaste" ? 'value-box value-box-active' : 'value-box'}
                         onMouseEnter={() => this.changeKey("valueTextZeroWaste")}
                         iconWhite={recycle}
                         id="valueZeroWaste" />
                     <ValueBox
-                        className={this.state.key == "valueTextEcoFriendly" ? 'value-box value-box-active' : 'value-box'}
+                        className={this.state.key === "valueTextEcoFriendly" ? 'value-box value-box-active' : 'value-box'}
                         onMouseEnter={() => this.changeKey("valueTextEcoFriendly")}
                         iconWhite={planet}
                         id="valueEcoFriendly" />
                 </div>
-
-
                 <CSSTransition
                     in={this.state.inProp}
                     timeout={800}
-                    classNames='my-node'
+                    classNames='appear'
                     unmountOnExit>
                     <div>
-                        <ReactCSSTransitionReplace transitionName="cross-fade"
+                        <ReactCSSTransitionReplace transitionName="replace"
                             transitionEnterTimeout={300} transitionLeaveTimeout={300}>
                             <div key={this.state.key} className='values-text' >
                                 <FormattedMessage id={this.state.key} />
