cfebeaec3b60df64e64b58b5ac354aada186e7ff
Hanna Trochimiuk <trochimiuk.hanna@gmail.com>
2020-07-13 21:32:42 +0200

    Updates

---
+++ precache-manifest.c494e4f3b7798abbdfb512697212f339.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "39c75e8f0fb3b91fc3300145882a7373",
+    "url": "/DesignCartel/static/media/people.39c75e8f.svg"
+  },
+  {
+    "revision": "f536d5b4264d75ea40bd",
+    "url": "/DesignCartel/static/css/main.a131f2a4.chunk.css"
+  },
+  {
+    "revision": "76acbd671c5701ab6b80",
+    "url": "/DesignCartel/static/js/runtime~main.d8acad37.js"
+  },
+  {
+    "revision": "0bcb34172e7a35d9bbf8",
+    "url": "/DesignCartel/static/js/2.42b91bb3.chunk.js"
+  },
+  {
+    "revision": "895980ed35fc73bda74dca3ed0805cc9",
+    "url": "/DesignCartel/static/media/back.895980ed.svg"
+  },
+  {
+    "revision": "d31bba5f2a03e01aad47203d17a58691",
+    "url": "/DesignCartel/static/media/logo.d31bba5f.jpg"
+  },
+  {
+    "revision": "2e07a741f9168655c2f8a3735a75f5be",
+    "url": "/DesignCartel/static/media/facebook.2e07a741.svg"
+  },
+  {
+    "revision": "c6d9f006a3a73761aad7a73c4d28a4d4",
+    "url": "/DesignCartel/static/media/instagram.c6d9f006.svg"
+  },
+  {
+    "revision": "5212ca696eb6faf807ff22fbdd2ad15f",
+    "url": "/DesignCartel/static/media/planet-earth.5212ca69.svg"
+  },
+  {
+    "revision": "0aafee990b39947cf3a1fdc40d8f9ff6",
+    "url": "/DesignCartel/static/media/design.0aafee99.svg"
+  },
+  {
+    "revision": "f536d5b4264d75ea40bd",
+    "url": "/DesignCartel/static/js/main.d03891ca.chunk.js"
+  },
+  {
+    "revision": "f015950715b42d18f7c61f79c1cd6df7",
+    "url": "/DesignCartel/static/media/recycle.f0159507.png"
+  },
+  {
+    "revision": "e78b5693c4a0566d67a622f154754eca",
+    "url": "/DesignCartel/static/media/serce.e78b5693.png"
+  },
+  {
+    "revision": "48bf832cd25e72db034d3c440201f4a4",
+    "url": "/DesignCartel/static/media/strzałka.48bf832c.png"
+  },
+  {
+    "revision": "247a293492ae58e04a3da6ba3f6d83ce",
+    "url": "/DesignCartel/static/media/korona.247a2934.png"
+  },
+  {
+    "revision": "b2a4b1c9d79fe1e455b9e72146843e2f",
+    "url": "/DesignCartel/static/media/słońce.b2a4b1c9.png"
+  },
+  {
+    "revision": "1c40544394f634a4178a4cac573a85ba",
+    "url": "/DesignCartel/static/media/logoSqr.1c405443.jpg"
+  },
+  {
+    "revision": "f406896e9f82b406d7e7785c1c0d57f4",
+    "url": "/DesignCartel/static/media/triangle.f406896e.jpg"
+  },
+  {
+    "revision": "4740d0bdf90472027aa7c83fbcc3e1b1",
+    "url": "/DesignCartel/static/media/header.4740d0bd.jpg"
+  },
+  {
+    "revision": "0ce0add0f7b904160cdb094354ace73c",
+    "url": "/DesignCartel/index.html"
+  }
+];
